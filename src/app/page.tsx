1d9436f02ee3e2de2f702a1f278c781604f1f006
fluidtype <maicholz@icloud.com>
2025-09-22 16:59:21 +0200

    Refine page transition layout for sticky CTA

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,9 +29,9 @@ export default function Home() {
           <FAQSection />
           <SectionSeparator />
           <FinalCTA />
-          <StickyCTABar />
         </div>
       </PageTransition>
+      <StickyCTABar />
     </div>
   );
 }
