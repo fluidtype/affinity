950093194c37ea83abb38ca92561bcdb3f621fb9
fluidtype <maicholz@icloud.com>
2025-09-25 18:24:03 +0200

    Prevent nested scroll container in main

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -70,7 +70,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         ) : null}
         <BackgroundGradient />
         <Header />
-        <main className="flex-1 overflow-x-hidden">{children}</main>
+        <main className="flex-1 min-w-0">{children}</main>
         <SiteFooter />
       </body>
     </html>
